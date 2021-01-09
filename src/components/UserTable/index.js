fef5e122f870bda31997a948273a1336e046328d
Ashvini Thomas <ashvinithomas@gmail.com>
2021-01-08 21:59:52 -0500

    created table in UserTable component

--- src/components/UserTable/index.js
+++ src/components/UserTable/index.js
@@ -1,15 +1,21 @@
 import React from "react";
 
-<table class="table">
-    <thead>
-        <tr>
-            <th scope="col">#</th>
-            <th scope="col">First</th>
-            <th scope="col">Last</th>
-            <th scope="col">Handle</th>
-        </tr>
-    </thead>
-    <tbody>
+function UserTable () {
+    return(
+        <div>
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">Image</th>
+                        <th scope="col">Name</th>
+                        <th scope="col">Phone</th>
+                        <th scope="col">Email</th>
+                        <th scope="col">DOB</th>
+                    </tr>
+                </thead>
+                <tbody></tbody>
+            </table>
+        </div>
+    )
+}
 
-    </tbody>
-</table>
