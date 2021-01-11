62282168058da1c87e72b84eaf0894d730ad91c6
Ashvini Thomas <ashvinithomas@gmail.com>
2021-01-11 10:14:32 -0500

    feat: minor changes

--- src/components/TopSearch/index.js
+++ src/components/TopSearch/index.js
@@ -28,6 +28,7 @@ export default function TopSearch() {
             {
                 label: 'Email',
                 field: 'email',
+                type: 'email',
                 width: 200,
             },
             {
